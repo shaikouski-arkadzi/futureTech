fe68cfc53c43fad7d43c622124de1bbdcf6ed4bb
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-04-23 22:06:33 +0300

    Add keyboard navigation for tabs

--- scripts/Tabs.js
+++ scripts/Tabs.js
@@ -57,15 +57,80 @@ class Tabs {
     });
   }
 
+  activateTab(newTabIndex) {
+    this.state.activeTabIndex = newTabIndex;
+    this.buttonElements[newTabIndex].focus();
+  }
+
+  previousTab = () => {
+    const newTabIndex =
+      this.state.activeTabIndex === 0
+        ? this.limitTabsIndex
+        : this.state.activeTabIndex - 1;
+
+    this.activateTab(newTabIndex);
+  };
+
+  nextTab = () => {
+    const newTabIndex =
+      this.state.activeTabIndex === this.limitTabsIndex
+        ? 0
+        : this.state.activeTabIndex + 1;
+
+    this.activateTab(newTabIndex);
+  };
+
+  firstTab = () => {
+    this.activateTab(0);
+  };
+
+  lastTab = () => {
+    this.activateTab(this.limitTabsIndex);
+  };
+
   onButtonClick(buttonIndex) {
     this.state.activeTabIndex = buttonIndex;
     this.updateUI();
   }
 
+  onKeyDown = (event) => {
+    const { code, metaKey } = event;
+    console.log(code);
+
+    const action = {
+      ArrowLeft: this.previousTab,
+      ArrowRight: this.nextTab,
+      Home: this.firstTab,
+      End: this.lastTab,
+    }[code]; // Select the appropriate function based on the key
+    // Suppose event.code === "ArrowLeft", then
+    // will called this.previousTab function
+
+    const isMacHomeKey = metaKey && code === "ArrowLeft";
+    if (isMacHomeKey) {
+      this.firstTab();
+      this.updateUI();
+      return;
+    }
+
+    const isMacEndKey = metaKey && code === "ArrowRight";
+    if (isMacEndKey) {
+      this.lastTab();
+      this.updateUI();
+      return;
+    }
+
+    if (action) {
+      action?.();
+      this.updateUI();
+    }
+  };
+
   bindEvents() {
     this.buttonElements.forEach((buttonElement, index) => {
       buttonElement.addEventListener("click", () => this.onButtonClick(index));
     });
+    this.rootElement.addEventListener("keydown", this.onKeyDown);
   }
 }
 
